6993f3fa5278ad71f84dd39fddf8b1d784b0cf66
DevSev <sigmafahri21@gmail.com>
2025-03-07 20:23:41 +0700

    Update windows-resize.bat

--- windows-resize.bat
+++ windows-resize.bat
@@ -1,7 +1,43 @@
 @echo off
+:: Cek apakah script sudah berjalan sebagai Administrator
+cd.>%windir%\GetAdmin
+if exist %windir%\GetAdmin (
+    del /f /q "%windir%\GetAdmin"
+) else (
+    echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\Admin.vbs"
+    "%temp%\Admin.vbs"
+    del /f /q "%temp%\Admin.vbs"
+    exit /b 2
+)
+
 mode con cp select=437 >nul
 
+:: Ambil drive utama (biasanya C:)
 set C=%SystemDrive:~0,1%
-for /f "tokens=2" %%a in ('echo list vol ^| diskpart ^| findstr "\<installer\>"') do (echo select vol %%a & echo delete partition) | diskpart
-for /f "tokens=2" %%a in ('echo list vol ^| diskpart ^| findstr "\<%C%\>"') do (echo select vol %%a & echo extend) | diskpart
+
+:: Hapus volume dengan label "installer"
+(
+    echo list volume
+    echo exit
+) | diskpart | findstr /i "installer" > temp.txt
+
+for /f "tokens=2" %%a in (temp.txt) do (
+    echo select volume %%a
+    echo delete volume
+) | diskpart
+del temp.txt
+
+:: Perpanjang volume utama
+(
+    echo list volume
+    echo exit
+) | diskpart | findstr /i "\<%C%\>" > temp2.txt
+
+for /f "tokens=2" %%a in (temp2.txt) do (
+    echo select volume %%a
+    echo extend
+) | diskpart
+del temp2.txt
+
+:: Hapus file batch ini setelah selesai
 del "%~f0"
